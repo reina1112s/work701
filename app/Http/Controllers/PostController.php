356eb8fa7e9496b938123407d47fcb78c306c594
reina1112s <adachi25shu@gmail.com>
2023-09-07 14:40:21 +0900

    詳細画面表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,13 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(Post $post){
-        return $post->get();
+    public function index(Post $post)
+    {
+        return view('posts.index')->with(['posts' => $post->get()]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス
+    }
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
     }
 }
